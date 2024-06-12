defbea879b7d1017b18ba76f8562e7d579f0739e
Freddisonrae <freddy.endler@gmail.com>
2024-06-12 22:50:54 +0200

    ieNOopen added

--- myApp.js
+++ myApp.js
@@ -8,10 +8,14 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
+ app.use(helmet.ieNoOpen());
 
 
 
+ 
 
+
+const ann = express()
 
 
 
