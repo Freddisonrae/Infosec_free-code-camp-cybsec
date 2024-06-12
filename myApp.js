cd75efea14948d8419004b9318adc9ec018b6915
Freddisonrae <freddy.endler@gmail.com>
2024-06-12 19:47:27 +0200

    Added the noSniff Helmet Action

--- myApp.js
+++ myApp.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 3030;
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter())
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 
 
 
