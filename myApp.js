02dd22b26985af372f32c886cf8a829290954d78
Freddisonrae <freddy.endler@gmail.com>
2024-06-12 23:00:03 +0200

    Added hsts to the programm

--- myApp.js
+++ myApp.js
@@ -8,14 +8,13 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
- app.use(helmet.ieNoOpen());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds = 90*24*60*60, force: true }))
 
 
 
  
 
-
-const ann = express()
 
 
 
